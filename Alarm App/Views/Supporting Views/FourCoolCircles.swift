c3a6ab39070a9c186717c7ee98c2a6d1a6fbd426
mehmet <mehmet.cel.ik@hotmail.com>
2025-03-14 14:01:38 +0300

    d

--- Alarm App/Views/Supporting Views/FourCoolCircles.swift
+++ Alarm App/Views/Supporting Views/FourCoolCircles.swift
@@ -7,6 +7,8 @@
 
 import SwiftUI
 
+let screenWidth = UIScreen.main.bounds.width
+let screenHeight = UIScreen.main.bounds.height
 struct CoolCircleView: View {
     
     let radius: CGFloat
@@ -39,7 +41,40 @@ struct FourCoolCircles: View {
     
     
     var body: some View {
-        Text("99 like icardi")
+        ZStack {
+            CoolCircleView(
+                radius: screenWidth / 10,
+                color1: color1,
+                color2: color2.opacity(0.3)
+            )
+            .offset(x: screenWidth / 4, y: -screenHeight / 4)
+            .offset(x: offsetX[0], y: offsetY[0])
+            
+            CoolCircleView(
+                radius: screenWidth / 7,
+                color1: color1,
+                color2: color2.opacity(0.3)
+            )
+            .offset(x: -screenWidth / 4, y: -screenHeight / 20)
+            .offset(x: offsetX[1], y: offsetY[1])
+            
+            CoolCircleView(
+                radius: screenWidth / 6,
+                color1: color1,
+                color2: color2.opacity(0.3)
+            )
+            .offset(x: (1/3) * screenWidth, y: screenHeight / 20)
+            .offset(x: offsetX[2], y: offsetY[2])
+            
+            CoolCircleView(
+                radius: screenWidth / 5,
+                color1: color1,
+                color2: color2.opacity(0.3)
+            )
+            .offset(x: -screenWidth / 3, y: screenHeight / 4)
+            .offset(x: offsetX[3], y: offsetY[3])
+            
+        }
     }
 }
 
