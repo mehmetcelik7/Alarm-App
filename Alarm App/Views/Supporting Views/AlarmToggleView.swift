12a99d33908f565cddf870067cf2cc389c952a51
mehmet <mehmet.cel.ik@hotmail.com>
2025-03-14 21:17:46 +0300

    j

---
+++ Alarm App/Views/Supporting Views/AlarmToggleView.swift
@@ -0,0 +1,18 @@
+//
+//  AlarmToggleView.swift
+//  Alarm App
+//
+//  Created by mehmet Çelik on 14.03.2025.
+//
+
+import SwiftUI
+
+struct AlarmToggleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AlarmToggleView()
+}
