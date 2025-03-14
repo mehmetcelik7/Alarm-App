04bbc257a7485f0742208d9e8afa334cd94a3675
mehmet <mehmet.cel.ik@hotmail.com>
2025-03-14 21:47:25 +0300

    k

---
+++ Alarm App/Views/Supporting Views/TimePicker.swift
@@ -0,0 +1,18 @@
+//
+//  TimePicker.swift
+//  Alarm App
+//
+//  Created by mehmet Çelik on 14.03.2025.
+//
+
+import SwiftUI
+
+struct TimePicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimePicker()
+}
